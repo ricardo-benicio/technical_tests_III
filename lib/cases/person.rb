20e6ee38209f652fa72bead05c16159fd030528c
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-06 16:01:34 -0300

    refactor: adding class Formatter in module Problem

--- lib/cases/person.rb
+++ lib/cases/person.rb
@@ -1,18 +1,47 @@
 # frozen_string_literal: true
+require 'yaml'
 
-class Person
-  def solution(input)
-    result = ""
+module Problem
+  class Person
+    def solution(input)
+      result = ""
 
-    input.each do |hash|
-      name = hash[:name][0, 11].ljust(11)
-      cpf = hash[:cpf][0, 11].ljust(11)
-      state = hash[:state][0, 11].ljust(11)
-      value = hash[:value][0, 11].ljust(11)
+      input.each do |hash|
+        name = hash[:name][0, 11].ljust(11)
+        cpf = hash[:cpf][0, 11].ljust(11)
+        state = hash[:state][0, 11].ljust(11)
+        value = hash[:value][0, 11].ljust(11)
 
-      result += "#{name}#{cpf}#{state}#{value}           \n"
+        result += "#{name}#{cpf}#{state}#{value}\n"
+      end
+
+      result
+    end
+  end
+
+  class Formatter
+    def initialize(format_file)
+      @format = YAML.load_file(format_file)
     end
 
-    result
+    def format(input)
+      result = ""
+
+      input.each do |hash|
+        formatted_hash = formatted_hash(hash)
+        result += "#{formatted_hash}\n"
+      end
+      result
+    end
+
+    private
+
+    def format_hash(hash)
+      formatted_values = @format.map do |field, length|
+        value = hash[field].to_s[0, length]
+        value.ljust(length)
+      end
+      formatted_values.join
+    end
   end
 end
