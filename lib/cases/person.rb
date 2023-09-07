e43caeef2f9a66857ebc5e7cba23c5294caefe5a
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-07 17:22:21 -0300

    refactor: rmv class Formatter

--- lib/cases/person.rb
+++ lib/cases/person.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
-require 'yaml'
 
-module Problem
   class Person
     def solution(input)
       result = ""
@@ -18,30 +16,3 @@ module Problem
       result
     end
   end
-
-  class Formatter
-    def initialize(format_file)
-      @format = YAML.load_file(format_file)
-    end
-
-    def format(input)
-      result = ""
-
-      input.each do |hash|
-        formatted_hash = formatted_hash(hash)
-        result += "#{formatted_hash}\n"
-      end
-      result
-    end
-
-    private
-
-    def format_hash(hash)
-      formatted_values = @format.map do |field, length|
-        value = hash[field].to_s[0, length]
-        value.ljust(length)
-      end
-      formatted_values.join
-    end
-  end
-end
