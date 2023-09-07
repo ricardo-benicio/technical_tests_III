c9bd802023e4ae3bf6fa1f15c02b54fbab665834
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-07 17:23:19 -0300

    test: refactoring problem conversion yaml

--- spec/cases/person_formatted_yaml_spec.rb
+++ spec/cases/person_formatted_yaml_spec.rb
@@ -2,22 +2,21 @@
 
 require "spec_helper"
 require "yaml"
-require "cases/person"
 
-RSpec.describe Problem do
+RSpec.describe YAML do
   let(:format_file) { "format.yaml" }
   let(:formatted) { Formatter.new(format_file) }
 
-  context "#solucao_format" do
+  context "#solution_format" do
     it "format correctly a single element of the array" do
       input = [{ name: "Maria Neusa de Aparecida",
                  cpf: "97905796671",
                  state: "Sao Paulo",
                  value: "1234" }]
 
-      expected_out = "Maria Neusa97905796671Sao Paulo  1234       \n"
+      expected_out = "97905796671Maria Neusa de00001234\n44010762900Ricardo Fontes00000567"
 
-      expect(Problem::Formatter.new(:format_file).format(input)).to eq(expected_out)
+      expect(formatted.format(input)).to eq(expected_out)
     end
 
     it "format correctly various elements of the array" do
@@ -27,7 +26,7 @@ RSpec.describe Problem do
       ]
       expected_output = "Maria Neusa   97905796671Sao Paulo    1234      \nRicardo Fonte44010762900Rio Grande do567       \n"
 
-      expect(Problem::Formatter.new.solution(input)).to eq(expected_output)
+      expect(Problem::Formatter.new(:format_file).solution(input)).to eq(expected_output)
     end
   end
 end
